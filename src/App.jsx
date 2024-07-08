11ba9480554ab295b65e9504e5240a2b670f4cb5
Sovan Bhakta <115566230+DevilKing25@users.noreply.github.com>
2024-07-09 01:22:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from './pages/ListAlbum/ListAlbum';
 import Sidebar from './components/Sidebar/Sidebar';
 import Navbar from './components/Navbar/Navbar';
 
-export const url = 'https://musicly-backend.onrender.com';
+export const url = 'https://devilking25.github.io/MUSICLY-backend/';
 
 const App = () => {
   return (
